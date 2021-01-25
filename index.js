4a87b3819334d3b19b8dc44c79c760b2f56fb814
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-01-25 10:11:04 -0800

    add inquirer handler functions

--- index.js
+++ index.js
@@ -4,29 +4,67 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 // 1. // predefine the 3 classes/constructor functions for the three types of employees : manager, engineer, intern - DONE
-
-const questions = [{
+const employeeQuestions = [{
     type: 'input',
     name: 'name',
-    message: 'Enter employee name.'
+    message: 'Enter employee name:'
 },
 {
     type: 'input',
     name: 'id',
-    message: 'Enter employee ID.'
+    message: 'Enter employee ID:'
 },
 {
     type: 'input',
     name: 'email',
-    message: 'Enter employee email.'
+    message: 'Enter employee email:'
 },
 {
     type: 'list',
     name: 'role',
-    message: 'Select employee role.',
+    message: 'Select employee role:',
     choices: ['manager', 'engineer', 'intern']
-}]
+}];
 
+const engineerQuestions = [{
+    type: 'input',
+    name: 'github',
+    message: 'Enter employee GitHub:'   
+}];
+
+const internQuestions = [{
+    type: 'input',
+    name: 'school',
+    message: 'Enter employee school:'   
+}];
+const managerQuestions = [{
+    type: 'input',
+    name: 'officeNumber',
+    message: 'Enter employee office number:'   
+}];
+
+async function main () {
+    const employeeAnswers = await inquirer.prompt(employeeQuestions);
+    const roleAnswers = await handleRoleQuestions(employeeAnswers.role);
+    console.log(employeeAnswers);
+    console.log(roleAnswers);
+};
+
+const handleRoleQuestions = (role) => {
+    switch (role) {
+        case 'manager':
+            return inquirer.prompt(managerQuestions);
+        case 'engineer':
+            return inquirer.prompt(engineerQuestions);
+        case 'intern':
+            return inquirer.prompt(internQuestions);         
+        default: 
+            console.log('oops');
+            break;
+    }
+}
+
+main();
 // 2. get info from the client about each employee to be added
 //    2.1 ask for name and other info of the manager
 //    2.2 ask if client wants to add another employee or exit
