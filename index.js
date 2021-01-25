e2f1dc35270ee2ee9b2f9dcb3336edd35d70f436
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-01-24 22:58:30 -0800

    add inquirer question object

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 // 1. // predefine the 3 classes/constructor functions for the three types of employees : manager, engineer, intern - DONE
+
+const questions = [{
+    type: 'input',
+    name: 'name',
+    message: 'Enter employee name.'
+},
+{
+    type: 'input',
+    name: 'id',
+    message: 'Enter employee ID.'
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Enter employee email.'
+},
+{
+    type: 'list',
+    name: 'role',
+    message: 'Select employee role.',
+    choices: ['manager', 'engineer', 'intern']
+}]
+
 // 2. get info from the client about each employee to be added
 //    2.1 ask for name and other info of the manager
 //    2.2 ask if client wants to add another employee or exit
